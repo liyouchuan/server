3ef6be0df925526dd3dad11a66b8c39e82f99779
ZxSeryNachuouy <34948261+ZxSeryNachuouy@users.noreply.github.com>
2018-01-03 15:02:57 -0800

    Update main.go

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+	fmt.Fprintf(w, "Hi there, there are %s!", r.URL.Path[1:])
 }
 
 func Port() string {
